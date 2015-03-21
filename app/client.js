673f5f2a3289caec3adc165b43b602bb0d218425
Neeraj Agarwal <neeraj.agarwal@olacabs.com>
2015-03-21 15:31:37 +0530

    working chat with chat_service server

--- app/client.js
+++ app/client.js
@@ -1,3 +1,11 @@
+var express = require("express");
+var app = express();
+var port = 3701;
+
+
+app.listen(port);
+console.log("Listening on port " + port);
+
 var io = require('socket.io/node_modules/socket.io-client');
 
 client = io.connect("http://localhost:3700");
@@ -7,5 +15,11 @@ client.on('connect',function() {
   client.emit("test","foo");
 }); 
 client.on('message', function(data) {
-  console.log('Received data: ' + JSON.stringify(data))
+  console.log('Received message on event: message. data: ' + JSON.stringify(data))
+});
+
+
+app.get("/", function(req, res){
+  // res.send(req.query);
+  client.emit(req.query.event ? req.query.event : "send", req.query.message ? req.query.message : "Test data my friend");
 });
